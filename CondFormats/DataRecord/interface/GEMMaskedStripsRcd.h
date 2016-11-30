e7f0ed9b3e8126f00d86fe2e1d4bfcdd13ad7efe
hyunyong <antilogy33@gmail.com>
2016-11-30 22:32:07 +0100

    gemMask

---
+++ CondFormats/DataRecord/interface/GEMMaskedStripsRcd.h
@@ -0,0 +1,29 @@
+#ifndef DataRecord_GEMMaskedStripsRcd_h
+#define DataRecord_GEMMaskedStripsRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     RPCMaskedStripsRcd
+// 
+/**\class RPCMaskedStripsRcd RPCMaskedStripsRcd.h CondFormats/DataRecord/interface/RPCMaskedStripsRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Wed Nov 03 15:48:55 CEST 2008
+// $Id: RPCMaskedStripsRcd.h,v 1.3 2008/11/03 18:55:37 sanabria Exp $
+//
+
+
+
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class GEMMaskedStripsRcd : public edm::eventsetup::EventSetupRecordImplementation<GEMMaskedStripsRcd> {};
+
+#endif
